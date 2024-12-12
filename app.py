682f8099f13868b51724f4f7f0791245bbb4052d
sumit <ben.sumit@gmail.com>
2024-12-12 14:57:54 +0530

    azure_streamlit_practice

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# Establish connection to Azure MySQL database
+def connect_to_db():
+    return mysql.connector.connect(
+        host='myfirst-mysql-server.mysql.database.azure.com',
+        user='sqldbadmin',
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=3306
+    )
+
+# Query data
+def get_data_from_db(query):
+    db_connection = connect_to_db()
+    cursor = db_connection.cursor(dictionary=True)
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    db_connection.close()
+    return result
+
+# Streamlit UI
+st.title('Azure MySQL with Streamlit')
+
+query = "SELECT * from mytable LIMIT 10;"  # Change query as needed
+data = get_data_from_db(query)
+
+if data:
+    st.write("Data from MySQL Database:")
+    for row in data:
+        st.write(f"{row['name']} has a :{row['pet']}:")
+else:
+    st.write("No data found.")
+

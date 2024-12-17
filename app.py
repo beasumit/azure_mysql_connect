4f3b3d392067482a124b183f2d20a76c761ee757
sumit <ben.sumit@gmail.com>
2024-12-18 01:28:12 +0530

    update

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ cnx = mysql.connector.connect(user=os.getenv("USERNAME"),
                                   host=os.getenv("SERVER"), 
                                   port=3306, 
                                   database=os.getenv("DATABASE"), 
-                                  ssl_ca="azure_certificate/DigiCertGlobalRootCA.crt.pem", 
-                                  ssl_disabled=True)
+                                  ssl_ca="azure_certificate/DigiCertGlobalRootCA.crt.pem")
 # Query data
 def get_data_from_db(query):
     db_connection = cnx
